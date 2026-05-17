5f97316dc04923bec46cf6baa89eb9bb2721cbc4
will1001 <wilirahmatm@gmail.com>
2026-05-17 22:56:09 +0800

    Expose donation schemas in Sanity Studio

--- cunindoensia/schemaTypes/index.ts
+++ cunindoensia/schemaTypes/index.ts
@@ -3,5 +3,7 @@ import dashboard from '../schemas/dashboard'
 import program from '../schemas/program'
 import gallery from '../schemas/gallery'
 import contact_profile from '../schemas/contact_profile'
+import donation from '../schemas/donation'
+import donation_record from '../schemas/donationRecord'
 
-export const schemaTypes = [dashboard, program, gallery, contact_profile]
+export const schemaTypes = [dashboard, program, gallery, contact_profile, post, donation, donation_record]
